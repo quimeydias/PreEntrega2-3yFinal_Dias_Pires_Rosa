d6ebbe0f5d29cb890d82b344038b1e14c6cdf42e
quimey <quimey098765@gmail.com>
2023-09-15 15:13:23 -0300

    ultima version

--- js/java.js
+++ js/java.js
@@ -4,78 +4,77 @@ const botonDerecha = document.querySelector(".derecha");
 const contenedor = document.querySelector(".tarjetas");
 
 
-botonIzquierda.addEventListener("click", () =>{
-contenedor.scrollLeft -=800;
+botonIzquierda.addEventListener("click", () => {
+    contenedor.scrollLeft -= 800;
 
 
 })
 
-botonDerecha.addEventListener("click", () =>{
-    contenedor.scrollLeft +=800;
-    
-    
-    })
-    
+botonDerecha.addEventListener("click", () => {
+    contenedor.scrollLeft += 800;
+
+
+})
 
 
 
 
-    const botonIzquierda1 = document.querySelector(".izquierda1");
-    const botonDerecha1 = document.querySelector(".derecha1");
-    
-    const contenedor1 = document.querySelector(".contenedorCategorias");
-    
-    
-    botonIzquierda1.addEventListener("click", () =>{
-    contenedor1.scrollLeft -=800;
-    
-    
-    })
-    
-    botonDerecha1.addEventListener("click", () =>{
-        contenedor1.scrollLeft +=800;
-        
-        
-        })
+
+const botonIzquierda1 = document.querySelector(".izquierda1");
+const botonDerecha1 = document.querySelector(".derecha1");
+
+const contenedor1 = document.querySelector(".contenedorCategorias");
+
+
+botonIzquierda1.addEventListener("click", () => {
+    contenedor1.scrollLeft -= 800;
+
+
+})
+
+botonDerecha1.addEventListener("click", () => {
+    contenedor1.scrollLeft += 800;
+
+
+})
 
 
 
 
-        const botonIzquierda2 = document.getElementById("izquierda");
-        const botonDerecha2 = document.getElementById("derecha");
-        
-        const contenedor2 = document.getElementById("tecnologia");
-        
-        
-        botonIzquierda2.addEventListener("click", () =>{
-        contenedor2.scrollLeft -=800;
-        
-        
-        })
-        
-        botonDerecha2.addEventListener("click", () =>{
-            contenedor2.scrollLeft +=800;
-            
-            
-            })
+const botonIzquierda2 = document.getElementById("izquierda");
+const botonDerecha2 = document.getElementById("derecha");
+
+const contenedor2 = document.getElementById("tecnologia");
 
 
-            const botonIzquierda3 = document.getElementById("izquierda1");
-        const botonDerecha3 = document.getElementById("derecha1");
-        
-        const contenedor3 = document.getElementById("tecnologia1");
-        
-        
-        botonIzquierda3.addEventListener("click", () =>{
-        contenedor3.scrollLeft -=800;
-        
-        
-        })
-        
-        botonDerecha3.addEventListener("click", () =>{
-            contenedor3.scrollLeft +=800;
-            
-            
-            })
-        
-    
+botonIzquierda2.addEventListener("click", () => {
+    contenedor2.scrollLeft -= 800;
+
+
+})
+
+botonDerecha2.addEventListener("click", () => {
+    contenedor2.scrollLeft += 800;
+
+
+})
+
+
+const botonIzquierda3 = document.getElementById("izquierda1");
+const botonDerecha3 = document.getElementById("derecha1");
+
+const contenedor3 = document.getElementById("tecnologia1");
+
+
+botonIzquierda3.addEventListener("click", () => {
+    contenedor3.scrollLeft -= 800;
+
+
+})
+
+botonDerecha3.addEventListener("click", () => {
+    contenedor3.scrollLeft += 800;
+
+
+})
+
